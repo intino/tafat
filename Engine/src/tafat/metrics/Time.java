5365a92f0121f7f8feaafb6e27184400bb21cb92
Octavio Roncal <octavioroncal11@gmail.com>
2015-04-24 07:27:39 +0100

    updated m2

---
+++ Engine/src/tafat/metrics/Time.java
@@ -0,0 +1,30 @@
+package tafat.metrics;
+
+import magritte.Metric;
+
+public enum Time implements Metric {
+
+	//Add the metrics here 
+	Seconds, Minutes;
+
+	@Override
+	public double value(double value) {
+		return converters[this.ordinal()].convert(value);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+	private static final Converter[] converters = { /* Add factors here */};
+
+	private static Converter factor(final double factor) {
+		return new Converter() {
+			@Override
+			public double convert(double value) {
+				return value * factor;
+			}
+		};
+	}
+}
