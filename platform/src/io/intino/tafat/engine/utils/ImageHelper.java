f72276abc53b1b9fb80f4df0dd87aebe40f62dc8
Octavio Roncal <octavioroncal@monentia.es>
2019-10-01 15:19:41 +0100

    changed Base64 library and updated tafat version

--- platform/src/io/intino/tafat/engine/utils/ImageHelper.java
+++ platform/src/io/intino/tafat/engine/utils/ImageHelper.java
@@ -4,14 +4,14 @@ import spark.utils.IOUtils;
 
 import java.io.IOException;
 import java.net.URL;
-
-import static com.sun.org.apache.xml.internal.security.utils.Base64.encode;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class ImageHelper {
 
 	public static String base64(URL image) {
 		try {
-			return encode(IOUtils.toByteArray(image.openStream()));
+			return Arrays.toString(Base64.getEncoder().encode(IOUtils.toByteArray(image.openStream())));
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
